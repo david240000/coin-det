7a5601710d99b84ef1c1f87462b72cbd2731a14a
György David <gyorgydavid2000@gmail.com>
2021-11-23 18:10:09 +0200

    Végső változat

--- coin.py
+++ coin.py
@@ -3,6 +3,42 @@ import cv2
 
 def sum(kep):
 
+        ermek={"5 HUF": {
+            "value": 5,
+            "radius": 21.5,
+            "ratio": 21.5/28.3,
+            "count": 0,
+        },
+        "10 HUF": {
+            "value": 10,
+            "radius": 24.8,
+            "ratio": 24.8/28.3,
+            "count": 0,
+        },
+        "20 HUF": {
+            "value": 20,
+            "radius": 26.3,
+            "ratio": 26.3/28.3,
+            "count": 0,
+        },
+        "50 HUF": {
+            "value": 50,
+            "radius": 27.4,
+            "ratio": 27.4/28.3,
+            "count": 0,
+        },
+        "100 HUF": {
+            "value": 100,
+            "radius": 23.8,
+            "ratio": 23.8/28.3,
+            "count": 0,
+        },
+        "200 HUF": {
+            "value": 200,
+            "radius": 28.3,
+            "ratio": 28.3/28.3,
+            "count": 0,
+        }}
         img = cv2.imread(kep)
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
@@ -15,7 +51,6 @@ def sum(kep):
             if largestRadius < i[2]:
                 largestRadius = i[2]
         #print(largestRadius)
-        print("Érmék:")
         change = 0
         for i in circles[0,:]:
                 cv2.circle(img,(i[0],i[1]),i[2],(0,255,0),2)
@@ -23,28 +58,33 @@ def sum(kep):
                 radius = i[2]
                 ratio = ((radius*radius) / (largestRadius*largestRadius))
                 tolerance=0.09
-                if(ratio >= (27.4/28.3)):
-                    change = change + 200
-                    print(200)
-                elif((ratio >=(26.3/28.3)) and (ratio<=(27.4/28.3))):
-                    change = change + 50
-                    print(50)
-                elif((ratio >= (24.8/28.3)-tolerance) and (ratio<(26.3/28.3))):
-                    change = change + 20
-                    print(20)
-                elif((ratio >= (23.8/28.3)-tolerance) and (ratio<(24.8/28.3)-tolerance)):
-                    change = change + 10
-                    print(10)
-                elif((ratio >= (21.5/28.3)-tolerance) and (ratio<(23.8/28.3)-tolerance)):
-                    change = change + 100
-                    print(100)
-                elif(ratio < (21.5/28.3)-tolerance):
-                    change = change + 5
-                    print(5)
+                if(ratio >= (ermek["50 HUF"]['ratio'])):
+                    change = change + ermek["200 HUF"]['value']
+                    ermek["200 HUF"]['count']+=1
+                elif((ratio >=(ermek["20 HUF"]['ratio'])) and (ratio<=(ermek["50 HUF"]['ratio']))):
+                    change = change + ermek["50 HUF"]['value']
+                    ermek["50 HUF"]['count']+=1
+                elif((ratio >= (ermek["10 HUF"]['ratio'])-tolerance) and (ratio<(ermek["20 HUF"]['ratio']))):
+                    change = change + ermek["20 HUF"]['value']
+                    ermek["20 HUF"]['count']+=1
+                elif((ratio >= (ermek["100 HUF"]['ratio'])-tolerance) and (ratio<(ermek["10 HUF"]['ratio'])-tolerance)):
+                    change = change + ermek["10 HUF"]['value']
+                    ermek["10 HUF"]['count']+=1
+                elif((ratio >= (ermek["5 HUF"]['ratio'])-tolerance) and (ratio<(ermek["100 HUF"]['ratio'])-tolerance)):
+                    change = change + ermek["100 HUF"]['value']
+                    ermek["100 HUF"]['count']+=1
+                elif(ratio < (ermek["5 HUF"]['ratio'])-tolerance):
+                    change = change + ermek["5 HUF"]['value']
+                    ermek["5 HUF"]['count']+=1
         print("Összeg :", change, " Ft")
+        print("Képen található érmék darabszám szerint:")
+        for erme in ermek:
+            darabszam = ermek[erme]['count']
+            print(f"{erme} = {darabszam} db")
+        print()
         font = cv2.FONT_HERSHEY_SIMPLEX
-        text = "Total value: " + str("%.2f" % round(change,2)) + " forint"
-        cv2.putText(img, text, (0,400), font, 0.5, (0, 0, 255), 2, cv2.LINE_AA)
+        text = "Osszertek: " + str("%.2f" % round(change,2)) + " forint"
+        cv2.putText(img, text, (0,400), font, 1, (255, 255, 255), 2, cv2.LINE_AA)
         cv2.imshow('Detected coins',img)
         cv2.waitKey()
 
@@ -52,7 +92,7 @@ def run_main():
     sum('forint.jpg')
     sum('kep.jpg')
     sum('kep1.jpg')
-
+    sum('kep2.jpg')
 
 if __name__ == "__main__":
     run_main()
